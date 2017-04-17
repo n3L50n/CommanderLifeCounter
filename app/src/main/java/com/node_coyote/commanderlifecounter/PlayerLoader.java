7f27dfd9f9c0cfc198e8e7f639cdf312b84b632b
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-17 16:47:35 -0700

    Create Settings activity. Begin implementing ViewStub.

---
+++ app/src/main/java/com/node_coyote/commanderlifecounter/PlayerLoader.java
@@ -0,0 +1,27 @@
+package com.node_coyote.commanderlifecounter;
+
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.os.Bundle;
+
+/**
+ * Created by node_coyote on 4/17/17.
+ */
+
+public class PlayerLoader implements LoaderManager.LoaderCallbacks {
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader loader, Object data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
+    }
+}
---
+++ app/src/main/java/com/node_coyote/commanderlifecounter/ResetPlayers.java
@@ -0,0 +1,11 @@
+package com.node_coyote.commanderlifecounter;
+
+/**
+ * Created by node_coyote on 4/17/17.
+ */
+
+/**
+ * Reset class to handle all reset methods
+ */
+public class ResetPlayers {
+}
